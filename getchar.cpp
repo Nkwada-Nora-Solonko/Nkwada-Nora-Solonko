68e1ec08871269455b95b32944b36abe0ba6cb39
Nkwada Nora Solonko <nkwadanora@gmail.com>
2023-04-17 15:17:18 -0700

    a program to print the End Of File value

---
+++ getchar.cpp
@@ -0,0 +1,17 @@
+// Verify that the expression getchar() != EOF is 0 or 1.
+// #include <stdio.h>
+
+// int main() {
+//         int c;
+//         printf( "%d\n",( getchar() != EOF));
+//         return 0;
+// }
+
+//program to print the value of EOF.
+#include <stdio.h>
+
+int main(void)
+{
+    printf("The value of EOF is %d.\n", EOF);
+    return 0;
+}
